5345c4e0922bcc13f6bbd4b412007db6aab5dcba
Developer/Tester <70017232+developertester786@users.noreply.github.com>
2026-05-13 10:53:54 +0530

    Initial Project Setup done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 id="title">WSS-AI-Review-QR-Platform</h1>
+     <p style={{textAlign: 'center'}}>Welcome to the WSS-AI-Review-QR-Platform! This platform is designed to facilitate the review process using AI and QR code technology. Stay tuned for updates and features coming soon!</p>
+    </>
+  )
+}
+
+export default App
